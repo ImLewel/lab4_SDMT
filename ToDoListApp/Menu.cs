3c20b940618398d6deebde4ca80aa53603c6afdc
Lev Pavelko <neroheroes6@gmail.com>
2023-05-31 19:28:33 +0300

    Added Menu and MenuOption classes
    Menu contains MenuOptions to navigate through program and do certain actions. Further updates will include combination with ToDoList logic

---
+++ ToDoListApp/Menu.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoListApp {
+  public class Menu : List<MenuOption> {
+    int selection;
+    int clearCount;
+    public void Render() {
+      int pos = 0;
+      clearCount = pos;
+      foreach (MenuOption option in this) {
+        Console.WriteLine($"{pos}. {option.label}");
+        ++pos;
+        ++clearCount;
+      }
+      OptionSelection();
+    }
+    int GetAnswer() {
+      Console.WriteLine("Choose an option:");
+      ++clearCount;
+      string answer = Console.ReadLine();
+      ++clearCount;
+      try {
+        selection = int.Parse(answer);
+      }
+      catch (Exception e) {
+        Console.WriteLine("No number provided, try again");
+        ++clearCount;
+        return GetAnswer();
+      }
+      if (selection >= this.Count || selection < 0) {
+        Console.WriteLine("Answer is bigger or less than any option in the list, try again");
+        ++clearCount;
+        return GetAnswer();
+      }
+      else
+        return selection;
+    }
+    void OptionSelection() {
+      selection = GetAnswer();
+      Console.CursorTop -= clearCount;
+      for (int _pos = 0; _pos < clearCount; ++_pos)
+        Console.WriteLine(new string(' ', Console.WindowWidth));
+      Console.CursorTop -= clearCount;
+      this[selection].action();
+    }
+  }
+  public class MenuOption {
+    public string label;
+    public Action action;
+    public MenuOption(string _label, Action _action) {
+      label = _label;
+      action = _action;
+    }
+  }
+}
--- ToDoListApp/Program.cs
+++ ToDoListApp/Program.cs
@@ -3,15 +3,7 @@
 namespace ToDoListApp {
   public class Program {
     static void Main(string[] args) {
-      ToDoList list = new();
-      list.Add(new TDTask("Do 1", DateTime.Now));
-      list.Add(new TDTask("Do 2", "Desc smth 2", DateTime.Now));
-      list.Add(new TDTask("Do 3", "Desc smth 3"));
-      list.Save();
-      list.Get();
-      foreach (TDTask task in list) {
-        Console.WriteLine($"Caption: {task.caption}, Description: {task.description}, Dead line: {task.deadLine}");
-      }
+
     }
   }
 }
